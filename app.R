3e839ef339b454d0f3b5d2937a7beb839bc70901
AmyJDavis <Amy.J.Davis@usda.gov>
2022-10-26 14:24:22 -0600

    Update app to only plot bad locations

--- app.R
+++ app.R
@@ -481,7 +481,7 @@ server <- function(input, output,session) {
     }
     data <- data()
     
-    df=data[,c("IDNUMBER","LONGITUDE","LATITUDE","N02","SPECIES","COUNTY","STATE")]
+    df=data[data$N02==1,c("IDNUMBER","LONGITUDE","LATITUDE","N02","SPECIES","COUNTY","STATE")]
     df=df[which(!is.na(df$LONGITUDE)),]
     loccols=c("black","red")[df$N02+1]
     
